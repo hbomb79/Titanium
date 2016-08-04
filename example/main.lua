ebb4b47b2e7d549303a3a1fce84f98c9c74d131b
hbomb79 <harryfelton12@gmail.com>
2016-08-04 20:08:28 +1200

    Changed example application to use node queries

--- example/main.lua
+++ example/main.lua
@@ -6,10 +6,19 @@ Manager = Application():set {
     terminatable = true
 }
 
-local masterTheme = Theme.fromFile( "masterTheme", "example/ui/master.theme" )
-Manager:addTheme( masterTheme )
-
 Manager:importFromTML "example/ui/master.tml"
+local app = {
+    masterTheme = Theme.fromFile( "masterTheme", "example/ui/master.theme" ),
+    sidePane = {
+        pane = Manager:query "Pane#pane",
+        hotkeys = Manager:query "Label.hotkey_part",
+
+        left = Manager:query "Label#left.hotkey_part",
+        right = Manager:query "Label#right.hotkey_part"
+    },
+}
+
+Manager:addTheme( app.masterTheme )
 Manager:getNode "exit_button":on("trigger", function( self )
     if RadioButton.getValue "rating" == "yes" then
         Manager:stop()
@@ -18,11 +27,7 @@ end)
 
 Manager:getNode "name_input":on("trigger", function( self, value, selectedValue )
     local nameDisplay = Manager:getNode "name_display"
-    if value == "" then
-        nameDisplay:removeClass "hasValue"
-    else
-        nameDisplay:addClass "hasValue"
-    end
+    nameDisplay:setClass( "hasValue", value ~= "" )
 
     nameDisplay.text = ( not value or value == "" ) and "No entered text" or ( #value > 40 and value:sub( 1, 40 ).."..." or value )
     Manager:getNode "selected_name_display".text = selectedValue and "Selected: "..selectedValue or "No selected text"
@@ -32,33 +37,26 @@ local themed = true
 Manager:getNode "toggle":on("trigger", function( self )
     themed = not themed
     if not themed then Manager:removeTheme "masterTheme"
-    else Manager:addTheme( masterTheme ) end
+    else Manager:addTheme( app.masterTheme ) end
 end)
 
-local leftCtrlLabel, pCharLabel, hyphenSeperator = Manager:getNode "left_ctrl_label", Manager:getNode "p_char_label", Manager:getNode "hyphen_seperator"
-local pane, paneStatus, currentAnimation = Manager:getNode "pane"
+local sidePane = app.sidePane
 local function paneToggle( isKey )
     paneStatus = not paneStatus
-    if currentAnimation then Manager:removeAnimation( currentAnimation ) end
+    if sidePane.anim then Manager:removeAnimation( sidePane.anim ) end
 
     if isKey == true then
-        leftCtrlLabel:addClass "active"
-        hyphenSeperator:addClass "active"
-        pCharLabel:addClass "active"
+        sidePane.hotkeys:addClass "active"
     end
 
-    currentAnimation = pane:animate("X", paneStatus and 32 or 52, paneStatus and 0.4 or 0.2, paneStatus and "outSine" or "inQuad", function()
-        leftCtrlLabel:removeClass "active"
-        pCharLabel:removeClass "active"
-        hyphenSeperator:removeClass "active"
+    sidePane.anim = sidePane.pane:animate("X", paneStatus and 32 or 52, paneStatus and 0.15 or 0.2, paneStatus and "outSine" or "inQuad", function()
+        sidePane.hotkeys:removeClass "active"
     end)
 end
 
-Manager:registerHotkey("close", "leftCtrl-leftShift-t", function()
-    Manager:stop()
-end)
+Manager:registerHotkey("close", "leftCtrl-leftShift-t", Manager.stop)
+Manager:getNode "pane_toggle":on("trigger", paneToggle)
 
-Manager:getNode "pane_toggle":on("trigger", function() paneToggle() end)
 Manager:registerHotkey("paneToggle", "leftCtrl-p", function()
     paneToggle( true )
 end)
@@ -67,11 +65,10 @@ Manager:addThread(Thread(function()
     while true do
         local event = coroutine.yield()
         if event.main == "KEY" then
-            local down = event.sub == "DOWN"
             if event.keyName == "leftCtrl" then
-                leftCtrlLabel[ down and "addClass" or "removeClass" ]( leftCtrlLabel, "held" )
+                sidePane.left:setClass( "held", event.sub == "DOWN" )
             elseif event.keyName == "p" then
-                pCharLabel[ down and "addClass" or "removeClass" ]( pCharLabel, "held" )
+                sidePane.right:setClass( "held", event.sub == "DOWN" )
             end
         end
     end
